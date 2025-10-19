cf1919d5fbe22c8ed084d9ec61b3184e39976f61
Seturaman Kumar <seturamankumar@gmail.com>
2025-10-19 17:55:29 +0530

    Fix: Made cookie accesible to backend and frontend

--- server/server.js
+++ server/server.js
@@ -37,6 +37,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
 }));
